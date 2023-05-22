0d822bf713400af3ac995a90da0c401f15df6b99
David Ferenczi <ferenczi.david55@gmail.com>
2023-05-22 19:54:01 +0200

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,7 @@ const Contact = () => {
                 <p className='py-6'>Submit the form to get in touch!</p>
             </div>
             
+            
             {/* Form component */}
             <div className='flex justify-center items-center'>
                 <form action="https://getform.io/f/13a04b81-6e5e-4188-b906-57476a4a2d45" method='POST' className='flex flex-col w-full md:w-1/2'>
@@ -18,6 +19,7 @@ const Contact = () => {
                         name='name' 
                         placeholder='Enter your name' 
                         className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+                        required
                     />
 
                     <input 
@@ -25,6 +27,7 @@ const Contact = () => {
                         name='email' 
                         placeholder='Enter your e-mail address' 
                         className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none my-4'
+                        required
                     />
 
                     <textarea 
@@ -32,8 +35,9 @@ const Contact = () => {
                         id="" 
                         rows="10" 
                         placeholder='Enter your message'
-                        className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'>
-                        
+                        className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+                        required
+                    >
                     </textarea>
                    
                     <button className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white group px-4 py-2 my-8 mx-auto flex items-center rounded  hover:scale-110 transition ease-in-out duration-300'>
