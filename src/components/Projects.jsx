83d5734dbd3685295f37a1edc1b925bd67909b11
David Ferenczi <ferenczi.david55@gmail.com>
2023-06-02 11:39:30 +0200

    new project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import tictactoe from '../assets/tictactoe.png'
+import cloudmanagement from '../assets/cloudmanagement.png'
 
 const Projects = () => {
 
   const projects = [
     {
       id:1,
-      src: tictactoe
-    }
+      src: tictactoe,
+      hrefGit: "https://github.com/ferenczidavid/tictactoe",
+      hrefDemo: "https://ferenczidavidtictactoe.netlify.app"
+    },
+    {
+      id:2,
+      src: cloudmanagement,
+      hrefGit: "https://github.com/ferenczidavid/data_services",
+      hrefDemo: "https://cmcloudmanagement.netlify.app"
+    } 
   ]
 
   return (
@@ -19,23 +28,25 @@ const Projects = () => {
             <p className='py-6'>Here are a few of my projects.</p>
         </div>
 
-        {
-          projects.map(({id,src}) => (
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-              <div className='shadow-md shadow-white rounded-lg'>
-                <img key={id} src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
-                <div className='flex items-center justify-center space-x-4'>
-                  <a href="https://github.com/ferenczidavid/tictactoe" target='_blank' rel='noopener noreferrer' >
-                    <button className='bg-slate-800 border border-gray-500 rounded-lg w-28 px-4 py-3 m-2 duration-200 hover:scale-105'>&lt;Code/&gt;</button>
-                  </a>
-                  <a href="https://jazzy-griffin-17f300.netlify.app" target='_blank' rel='noopener noreferrer' >
-                    <button className='bg-slate-800 border border-gray-500 rounded-lg w-28 px-4 py-3 m-2 duration-200 hover:scale-105'>&lt;Demo/&gt;</button>
-                  </a>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+          {
+            projects.map(({id,src,hrefGit,hrefDemo}) => (
+              
+                <div className='shadow-md shadow-white rounded-lg'>
+                  <img key={id} src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
+                  <div className='flex items-center justify-center space-x-4'>
+                    <a href={hrefGit} target='_blank' rel='noopener noreferrer' >
+                      <button className='bg-slate-800 border border-gray-500 rounded-lg w-28 px-4 py-3 m-2 duration-200 hover:scale-105'>&lt;Code/&gt;</button>
+                    </a>
+                    <a href={hrefDemo} target='_blank' rel='noopener noreferrer' >
+                      <button className='bg-slate-800 border border-gray-500 rounded-lg w-28 px-4 py-3 m-2 duration-200 hover:scale-105'>&lt;Demo/&gt;</button>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))
-        }
+              
+            ))
+          }
+        </div>
       </div>
     </div>
   )
